27954f5ea3a6869103049902c7998a92e4c86fbc
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-12-24 22:52:01 +0200

    Initial Commit

---
+++ Buttons/ButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonsApp.swift
+//  Buttons
+//
+//  Created by Vahtee Boo on 24.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct ButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
