44736f3a30ed1b643c2509dd536ebeb41772492d
Vahtee Boo <ronnie.kaa@bigmir.net>
2021-12-24 22:57:34 +0200

    Initial

--- Buttons/ContentView.swift
+++ Buttons/ContentView.swift
@@ -9,8 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Button("Button 1") {}
+            .buttonStyle(.bordered)
+            Button("Button 2", role: .cancel) {}
+            .buttonStyle(.bordered)
+            .tint(.yellow)
+            Button("Button 3", role: .destructive) {}
+            .buttonStyle(.bordered)
+            Button("Button 4") {}
+            .buttonStyle(.borderedProminent)
+            Button("Button 5", role: .cancel) {}
+            .buttonStyle(.borderedProminent)
+            .tint(.green)
+            Button("Button 6", role: .destructive) {}
+            .buttonStyle(.borderedProminent)
+        }
     }
 }
 
